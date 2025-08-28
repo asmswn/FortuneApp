f73b3e6bdf5a3ac6af30efad1e3269c0250a2ba4
asata <asata@MSI>
2025-08-28 11:20:25 +0900

    initial commit

---
+++ src/main/java/com/example/demo/UranaiWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UranaiWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UranaiWebApplication.class, args);
+	}
+
+}
